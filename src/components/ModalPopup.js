6568ca64cc1fb24899e6d1ab219944579c397f2c
Vikramchandramouli <96676549+Vikramchandramouli@users.noreply.github.com>
2022-06-17 16:44:08 +0530

    initial commit

---
+++ src/components/ModalPopup.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { Modal } from "antd";
+
+const ModalPopup = ({
+  title,
+  visible,
+  toggle,
+  footer,
+  height,
+  width,
+  content,
+}) => {
+  return (
+    <Modal
+      title={"Hello"}
+      visible={visible}
+      onCancel={toggle}
+      footer={footer}
+      height={height}
+      width={width}
+      content={content}
+    >
+      {content}
+    </Modal>
+  );
+};
+
+ModalPopup.defaultProps = {
+  title: "Title",
+  visible: false,
+  footer: "Footer",
+  height: "auto",
+  width: "50vw",
+  content: <p>Content</p>,
+};
+
+export default ModalPopup;
